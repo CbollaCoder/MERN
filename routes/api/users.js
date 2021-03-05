b4899a591fe990abe27472a3efcb75ed3065cb86
America Rivas <amerivasmur@gmail.com>
2021-03-05 13:21:34 -0400

    Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 const { check, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
@@ -60,10 +63,21 @@ async (req,res) => {
 
     await user.save();
 
-    //Return jsonwebtoken
-    res.send('User registered');
+    const payload = {
+        user:{
+            id: user.id
+        }
+     }
 
-
+     jwt.sign(
+         payload,
+         config.get('jwtSecret'),
+         {expiresIn: 360000},
+         (err, token) => {
+             if(err) throw err;
+             res.json({ token });
+        }
+    );
     }catch(err){
         console.error(err.message);
         res.status(500).send('Server error');
