be082b3014c3dacbd0a5af47d55e9d9e01e92b21
America Rivas <amerivasmur@gmail.com>
2021-03-05 17:10:42 -0400

    User Authentication/Login Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -81,10 +81,8 @@ async (req,res) => {
     }catch(err){
         console.error(err.message);
         res.status(500).send('Server error');
-    }
-   
-        
-    }
+    }    
+}
 );
 
 module.exports = router;
